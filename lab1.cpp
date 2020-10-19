a47d45a7f63f02cd3b9625b1a1f3f2586620c472
Tsukimi <1027532960@qq.com>
2020-10-19 15:21:11 +0800

    去除注释

--- lab1.cpp
+++ lab1.cpp
@@ -14,24 +14,21 @@ typedef enum
 } frame_kind;
 typedef struct frame_head
 {
-	frame_kind kind;		 // 帧类型
-	unsigned int seq;		 // 序列号
-	unsigned int ack;		 // 确认号
-	unsigned char data[100]; // 数据
+	frame_kind kind;
+	unsigned int seq;
+	unsigned int ack;
+	unsigned char data[100];
 };
 typedef struct frame
 {
-	frame_head head;   // 帧头
-	unsigned int size; // 数据的大小
+	frame_head head;
+	unsigned int size;
 };
 
 std::deque<frame *> appending_queue;
 int send_win = 0;
 int receive_win = 0;
 
-/*
-* 停等协议测试函数
-*/
 int stud_slide_window_stop_and_wait(char *pBuffer, int bufferSize, UINT8 messageType)
 {
 	frame *frame = new frame;
@@ -80,17 +77,11 @@ int stud_slide_window_stop_and_wait(char *pBuffer, int bufferSize, UINT8 message
 	return 0;
 }
 
-/*
-* 回退n帧测试函数
-*/
 int stud_slide_window_back_n_frame(char *pBuffer, int bufferSize, UINT8 messageType)
 {
 	return 0;
 }
 
-/*
-* 选择性重传测试函数
-*/
 int stud_slide_window_choice_frame_resend(char *pBuffer, int bufferSize, UINT8 messageType)
 {
 	return 0;
